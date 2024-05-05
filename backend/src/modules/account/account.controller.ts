e8ca216e8ab0831125481013c72233ce07936e18
Thiago Cordeiro <thiagoty52@gmail.com>
2024-05-05 11:53:12 -0300

    fix[ts-8]: Correct lack of validation for missing account number in API requests

--- backend/src/modules/account/account.controller.ts
+++ backend/src/modules/account/account.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Body, Get, HttpStatus, Post, Param } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  HttpStatus,
+  Post,
+  Param,
+  BadRequestException,
+} from '@nestjs/common';
 import { AccountService } from './account.service';
 
 @Controller('account')
@@ -6,6 +14,10 @@ export class AccountController {
   constructor(private readonly accountService: AccountService) {}
   @Post('create')
   async createAccount(@Body('number') number: string) {
+    if (!number) {
+      throw new BadRequestException('Account number is required.');
+    }
+
     const account = await this.accountService.createAccount(Number(number));
     return {
       status: HttpStatus.CREATED,
